5a820bbfe2e65dd39d7662e054f590ed04c98fab
kaia <fedex0329l@gmail.com>
2025-08-29 19:58:26 +0800

    impl

---
+++ src/main/java/com/gtelant/commerce_servise/CommerceServiseApplication.java
@@ -0,0 +1,13 @@
+package com.gtelant.commerce_servise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceServiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtelant/commerce_servise/CommerceServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtelant.commerce_servise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceServiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
